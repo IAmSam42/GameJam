583cd1195404b9f83d36a6a636e3b6796d573cc9
Sam Holdcroft <sam.holdcroft42@gmail.com>
2016-06-12 07:49:13 +0100

    Search algorithm now correctly places the start position into the Priority Queue

--- UpJam/src/engine/ai/SearchAlgorithm.java
+++ UpJam/src/engine/ai/SearchAlgorithm.java
@@ -1,7 +1,5 @@
 package engine.ai;
 
-import java.util.PriorityQueue;
-
 import map.Map;
 
 public class SearchAlgorithm 
@@ -10,23 +8,47 @@ public class SearchAlgorithm
 	private TilePosition start;
 	private TilePosition goal;
 	
-	private PriorityQueue<TilePath> positionQueue;
+	private PriorityQueue positionQueue;
 			
 	public SearchAlgorithm(Map map, TilePosition start, TilePosition goal) 
 	{
 		this.map = map;
 		this.start = start;
 		this.goal = goal;
+		
+		positionQueue = new PriorityQueue();
 	}
 
 	public void setStart(TilePosition start)
 	{
 		this.start = start;
+		positionQueue.clear();
+		makeFirstElem();
 	}
 	
 	public void setGoal(TilePosition goal)
 	{
 		this.goal = goal;
+		positionQueue.clear();
+		makeFirstElem();
+	}
+	
+	private void makeFirstElem()
+	{
+		TilePath startPath = new TilePath(start);
+		double weight = getHValue(start, startPath);
+		
+		positionQueue.put(start, startPath, weight);
+	}
+	
+	private double getHValue(TilePosition pos, TilePath path)
+	{
+		int distSqr = ((goal.getX() - pos.getX())*(goal.getX() - pos.getX())) + ((goal.getY() - pos.getY())*(goal.getY() - pos.getY()));
+		double dist = Math.sqrt((double)distSqr); 
+		
+		double pathLength = (double)path.length();
+		
+		return dist + pathLength;
 	}
 	
 	public void search()
