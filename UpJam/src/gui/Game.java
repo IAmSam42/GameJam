eb410f0d9eac757142870f23e4fde76a95a0905b
Sam Holdcroft <sam.holdcroft42@gmail.com>
2016-06-12 21:33:44 +0100

    Changed where the robot spawns

--- UpJam/src/gui/Game.java
+++ UpJam/src/gui/Game.java
@@ -51,7 +51,7 @@ public class Game extends Canvas implements Runnable{
 		Player player = new Player(32,32, Tile.TILESIZE, handler, map);
 		//handler.addPlayer(player);
 
-		handler.addPlayer(new Robot(100, 100, Tile.TILESIZE, map, player, handler.getExtras()));
+		handler.addPlayer(new Robot(384, 192, Tile.TILESIZE, map, player, handler.getExtras()));
 
 		this.addMouseListener(new MouseListener() {
 			
