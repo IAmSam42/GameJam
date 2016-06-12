dfd8af8e640fc79ad7771d102b8573db8671c13f
Sam Holdcroft <sam.holdcroft42@gmail.com>
2016-06-12 07:48:06 +0100

    Created a class to store a path of tile positions

--- UpJam/src/engine/ai/TilePath.java
+++ UpJam/src/engine/ai/TilePath.java
@@ -1,8 +1,36 @@
 package engine.ai;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 
 public class TilePath 
 {
 	private ArrayList<TilePosition> path;
+	
+	public TilePath()
+	{
+		path = new ArrayList<TilePosition>();
+	}
+	
+	public TilePath(TilePosition firstPos)
+	{
+		path = new ArrayList<TilePosition>();
+		path.add(firstPos);
+	}
+	
+	public void add(TilePosition newPos)
+	{
+		path.add(newPos);
+	}
+	
+	public int length()
+	{
+		return path.size();
+	}
+	
+	public Deque<TilePosition> getStack()
+	{
+		return new ArrayDeque<TilePosition>(path);
+	}
 }
