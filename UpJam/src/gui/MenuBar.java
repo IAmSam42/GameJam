a7652622467b99bb04cd293a01f2b75a7ee92d33
Helena Bower <Helena Bower>
2016-06-12 13:03:20 +0100

    Started working on menu bar

---
+++ UpJam/src/gui/MenuBar.java
@@ -0,0 +1,75 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridLayout;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class MenuBar {
+	JPanel p;
+	JFrame frame;
+	static JTextField noTraps;
+
+	public MenuBar(int width, int height) {
+		frame = new JFrame("28 Robots Later");
+		frame.setSize(width, height+100);
+		frame.getContentPane().setLayout(null);
+		frame.setBackground(Color.LIGHT_GRAY);
+		frame.setLocationRelativeTo(null);
+
+		
+		p = new JPanel();
+		p.setSize(width, height);
+		p.setBackground(Color.LIGHT_GRAY);
+		p.setLayout(new GridLayout(1, 4));
+		frame.getContentPane().add(p);
+
+		// Import font
+		try {
+			GraphicsEnvironment ge = GraphicsEnvironment
+					.getLocalGraphicsEnvironment();
+			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(
+					"resources/font/prstartk.ttf")));
+		} catch (IOException | FontFormatException e) {
+			// Handle exception
+		}
+		// Create font for buttons, size for title is different
+		Font font = new Font("Press Start K", Font.PLAIN, 16);
+
+		// Get blue colour
+		Color blueColour = new Color(27, 14, 89);
+
+		JButton b = new JButton("Menu");
+		b.setSize(width / 10, 25);
+		b.setFont(font);
+		b.setForeground(Color.WHITE);
+		b.setBackground(blueColour);
+		p.add(b);
+		
+		JLabel trapsLabel = new JLabel("No. of Traps: ");
+		trapsLabel.setFont(new Font("Press Start K", Font.PLAIN, 12));
+		trapsLabel.setSize(width / 5, 25);
+		trapsLabel.setForeground(blueColour);
+		p.add(trapsLabel);
+		
+		noTraps = new JTextField();
+		noTraps.setEditable(false);
+		noTraps.setBackground(Color.LIGHT_GRAY);
+		p.add(noTraps);
+		
+		frame.setVisible(true);
+	}
+	
+	public static void updateTraps(int no){
+		noTraps.setText("" + no);
+	}
+}
